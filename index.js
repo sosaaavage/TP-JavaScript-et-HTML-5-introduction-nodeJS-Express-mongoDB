b8741714c86447d3f8e84ba05bf0ed0466eba8b9
Anthony Gomez <anthony.gomez@koedia.com>
2018-11-29 17:29:38 +0100

    add POST restaurant

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const port = 3000;
 
 var express = require ("express");
- 
-var app = express ();
+var bodyParser = require("body-parser");
+
+var app = express();
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // routage
     
 app.get("/api", (req, res, next) => { //return all restaurants
     res.json(restaurants);
-   });
+});
+
+app.post("/api", (req,res) => {
+    var r_nom = req.query.nom;
+    var r_cuisine = req.query.cuisine;
+    
+    try{
+        restaurants.push(
+            {
+                nom: r_nom,
+                cuisine: r_cuisine
+            }
+        );
+        res.end("OK");
+    }
+    catch(e){
+        res.end("ERROR "+e);
+    }
+});
 
 // init restaurants datas
 var restaurants = [];
