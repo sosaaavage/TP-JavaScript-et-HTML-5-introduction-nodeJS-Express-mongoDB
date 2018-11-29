21e136c5051bd5c61643a4681baaf012b4acb29c
Anthony Gomez <gantho06@yahoo.fr>
2018-11-29 23:31:39 +0100

    add more restaurants datas on initialization + add check undefined values in put|update verb

--- server/index.js
+++ server/index.js
@@ -12,19 +12,168 @@ app.use(bodyParser.json());
 // init restaurants datas
 var restaurants = [];
 restaurants.push(
-    {
-        nom: 'Café de Paris',
-        cuisine: 'Française'
-    },
-    {
-        nom: 'Sun City Café',
-        cuisine: 'Américaine'
-    }
+        {
+          "nom": "française",
+          "cuisine": "Insuron"
+        },
+        {
+          "nom": "gastronomique",
+          "cuisine": "Geofarm"
+        },
+        {
+          "nom": "corse",
+          "cuisine": "Evidends"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Artiq"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Exozent"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Trasola"
+        },
+        {
+          "nom": "corse",
+          "cuisine": "Radiantix"
+        },
+        {
+          "nom": "bretonne",
+          "cuisine": "Poochies"
+        },
+        {
+          "nom": "française",
+          "cuisine": "Quarmony"
+        },
+        {
+          "nom": "italienne",
+          "cuisine": "Exiand"
+        },
+        {
+          "nom": "bretonne",
+          "cuisine": "Concility"
+        },
+        {
+          "nom": "bretonne",
+          "cuisine": "Silodyne"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Darwinium"
+        },
+        {
+          "nom": "marocaine",
+          "cuisine": "Eplosion"
+        },
+        {
+          "nom": "italienne",
+          "cuisine": "Kyagoro"
+        },
+        {
+          "nom": "marocaine",
+          "cuisine": "Ontagene"
+        },
+        {
+          "nom": "espagnole",
+          "cuisine": "Egypto"
+        },
+        {
+          "nom": "niçoise",
+          "cuisine": "Bizmatic"
+        },
+        {
+          "nom": "mexicaine",
+          "cuisine": "Tropolis"
+        },
+        {
+          "nom": "gastronomique",
+          "cuisine": "Xleen"
+        },
+        {
+          "nom": "française",
+          "cuisine": "Zomboid"
+        },
+        {
+          "nom": "gastronomique",
+          "cuisine": "Aquoavo"
+        },
+        {
+          "nom": "niçoise",
+          "cuisine": "Snips"
+        },
+        {
+          "nom": "niçoise",
+          "cuisine": "Bitrex"
+        },
+        {
+          "nom": "corse",
+          "cuisine": "Otherside"
+        },
+        {
+          "nom": "corse",
+          "cuisine": "Bitendrex"
+        },
+        {
+          "nom": "mexicaine",
+          "cuisine": "Bunga"
+        },
+        {
+          "nom": "espagnole",
+          "cuisine": "Waab"
+        },
+        {
+          "nom": "bretonne",
+          "cuisine": "Geekol"
+        },
+        {
+          "nom": "gastronomique",
+          "cuisine": "Izzby"
+        },
+        {
+          "nom": "niçoise",
+          "cuisine": "Isotronic"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Eclipto"
+        },
+        {
+          "nom": "française",
+          "cuisine": "Assistia"
+        },
+        {
+          "nom": "américaine",
+          "cuisine": "Dadabase"
+        },
+        {
+          "nom": "mexicaine",
+          "cuisine": "Telpod"
+        },
+        {
+          "nom": "bretonne",
+          "cuisine": "Prismatic"
+        },
+        {
+          "nom": "française",
+          "cuisine": "Portaline"
+        },
+        {
+          "nom": "espagnole",
+          "cuisine": "Cujo"
+        },
+        {
+          "nom": "niçoise",
+          "cuisine": "Techmania"
+        }
 );
 
 // routage
     
 app.get("/api/restaurants", (req, res, next) => { //retrieve all restaurants
+    console.log("[GET] restaurants");
     res.json(restaurants);
 });
 
@@ -32,6 +181,7 @@ app.post("/api/restaurant", (req,res) => { //create a restaurant
     try{
         var r_nom = req.body.nom;
         var r_cuisine = req.body.cuisine;
+        if(typeof r_nom != "undefined" && r_cuisine != "undefined"){
             restaurants.push(
                 {
                     nom: r_nom,
@@ -40,6 +190,11 @@ app.post("/api/restaurant", (req,res) => { //create a restaurant
             );
             console.log("[POST-CREATE] restaurant : "+r_nom+" "+r_cuisine);
             res.end("OK");
+        }
+        else{
+            res.end("ERROR : undefined values into body");
+        }
+            
     }
     catch(e){
         res.end("ERROR "+e);
@@ -62,6 +217,9 @@ app.put("/api/restaurant/:id",(req,res) => {
         if(typeof r_nom !== "undefined"){
             restaurant_selected.cuisine = r_cuisine;
             log = log + " cuisine updated, new value : "+r_cuisine;
+        }
+        else if(r_nom !== "undefined" && r_cuisine !== "undefined"){
+            res.end("ERROR : undefined values into body");
         }
         console.log("[PUT-UPDATE] restaurant id "+id+" -> "+log);
         res.end("OK");
