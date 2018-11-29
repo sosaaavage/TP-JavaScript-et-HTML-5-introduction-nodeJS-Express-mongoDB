a95a1cbc0dbe186acd0ec9e85a7c1628f90aecb2
Anthony Gomez <anthony.gomez@koedia.com>
2018-11-29 16:49:21 +0100

    add restaurants list variable and init this

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ var express = require ("express");
  
 var app = express ();
 
-app.get("/api", (req, res, next) => {
-    res.json([
-        {
-            nom: 'café de Paris',
-            cuisine: 'Française'
-        },
-        {
-            nom: 'Sun City Café',
-            cuisine: 'Américaine'
-        }
-    ]);
+// routage
+    
+app.get("/api", (req, res, next) => { //return all restaurants
+    res.json(restaurants);
    });
 
+// init restaurants datas
+var restaurants = [];
+restaurants.push(
+    {
+        nom: 'Café de Paris',
+        cuisine: 'Française'
+    },
+    {
+        nom: 'Sun City Café',
+        cuisine: 'Américaine'
+    }
+);
+
+// start server
 app.listen (port, () => { 
     console.log ("Serveur fonctionnant sur le port "+port); 
 });
